bc5d429b4b414b2a47f9d2b1d3dd9b6e865376bd
Fung Chow <fungchow@gmail.com>
2022-05-13 00:15:31 +0100

    initial commit - controller test file is created.

---
+++ src/test/java/com/techReturner/weatherAPI/WeatherAPIControllerTest.java
@@ -0,0 +1,28 @@
+package com.techReturner.weatherAPI;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.techreturners.bookmanager.model.Book;
+import com.techreturners.bookmanager.model.Genre;
+import com.techreturners.bookmanager.service.BookManagerServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+public class WeatherAPIControllerTest {
+}
